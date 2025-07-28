9e0da5eb9add2fc229881513deeecb315785dee1
Javeeth Nasim <javeeth@vsatechsolutions.com>
2025-07-28 17:12:29 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' })
 })
 
+//Crud Funtionality
+
+
+app.get('/movies', () => {
+
+})
+
+app.post('/movies', () => {
+    
+})
+
+
+app.put('/movies/:id', () => {
+    
+})
+
+
+app.delete('/movies/:id', () => {
+    
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 })
