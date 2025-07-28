e267a48e5d9a387d050ac80f70f68dcea34d556a
Javeeth Nasim <javeeth@vsatechsolutions.com>
2025-07-28 17:06:28 +0530

    feat: App structure

---
+++ README.md
@@ -0,0 +1,13 @@
+# moviesareas-api app
+
+provide a movie api for a react  js app
+
+### Important commands
+
+1. `npm install`
+2. `npm run server`
+3. `git init`
+4. `git add`
+5. `git commit -m "Your MSG"`
+6. `git push -u origin main`
+7. `git push`
---
+++ main.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
+//Dry Principle: Keep your code DRY (Don't Repeat Yourself) by creating reusable functions and modules.
+//Kiss Principle: Keep your code simple and straightforward. Avoid unnecessary complexity.
+//Yagni Principle: You Aren't Gonna Need It. Don't add features until they are necessary.
